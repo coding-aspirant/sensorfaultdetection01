566eba0190235246b0b98c402fdce537bfa23236
Lokes <shyamsundersharma7979@gmail.com>
2025-03-16 20:05:50 +0530

    added module

---
+++ src/constant/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+AWS_S3_BUCKET_NAME = "Wafer-fault"
+MONGO_DATABASE_NAME = "pwskills"
+MONGO_COLLECTION_NAME = "Wafer-fault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = " "
+MODEL_FILE_NAME = "model"
+MONGO_FILE_EXTENSION = ".pkl"
+
